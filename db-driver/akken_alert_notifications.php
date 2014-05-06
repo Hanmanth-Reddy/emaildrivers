dae26a015440e4bf82da8ad6700a82a5e27b4aea
prasada <prasada@755fa31f-b065-4e3e-93bf-f78d3898b98b>
2014-05-06 06:01:46 +0000

    committing AKKENED_7_5_0_25 tag in to trunk

--- db-driver/akken_alert_notifications.php
+++ db-driver/akken_alert_notifications.php
@@ -9,7 +9,7 @@
 
 	if($meteor)
 	{
-		$dque="select capp_info.comp_id from company_info LEFT JOIN capp_info ON capp_info.sno=company_info.sno LEFT JOIN options ON options.sno=company_info.sno where (options.notifcations='Y' OR options.vms='Y') AND company_info.status='ER' ".$version_clause;
+		$dque="select capp_info.comp_id from company_info LEFT JOIN capp_info ON capp_info.sno=company_info.sno LEFT JOIN options ON options.sno=company_info.sno where company_info.status='ER' ".$version_clause;
 		$dres=mysql_query($dque,$maindb);
 		while($drow=mysql_fetch_row($dres))
 		{
@@ -19,7 +19,7 @@
 			$cque="SELECT c.username 
 			FROM akken_notifications.notifications p 
 			LEFT JOIN akken_notifications.notifications_list c ON p.sno=c.psno 
-			WHERE c.username>0 AND c.alert_status='N' GROUP BY c.username";
+			WHERE p.alert='Y' AND c.username>0 AND c.alert_status='N' GROUP BY c.username";
 			$cres=mysql_query($cque,$db);
 			while($crow=mysql_fetch_row($cres))
 			{
@@ -30,7 +30,7 @@
 				$nque="SELECT p.title, p.descri, c.sno 
 				FROM akken_notifications.notifications p 
 				LEFT JOIN akken_notifications.notifications_list c ON p.sno=c.psno 
-				WHERE c.username>0 AND c.alert_status='N' AND c.username='$username' ORDER BY p.cdate";
+				WHERE p.alert='Y' AND c.alert_status='N' AND c.username='$username' ORDER BY p.cdate";
 				$nres=mysql_query($nque,$db);
 				while($nrow=mysql_fetch_row($nres))
 				{
