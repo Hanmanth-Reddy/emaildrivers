5bfe98db8caa036b4c9805208192d8c527bc7874
prasada <prasada@755fa31f-b065-4e3e-93bf-f78d3898b98b>
2014-12-22 05:24:56 +0000

    committing AKKENED_7_5_0_47 tag in to trunk

--- db-driver/akken_email_alerts_jobboards.php
+++ db-driver/akken_email_alerts_jobboards.php
@@ -54,40 +54,79 @@
 		
 		if(count($recent_jo_arr) > 0){
 			// Find all applicants coming from job boards [sourcetype = 169 i.e website]
-			$applicant_search_query = "SELECT
-							api_consultant_list.username,
+			// Also, all applicants subscribed through talentpool 
+			$applicant_search_query = "SELECT							
 							api_consultant_list.name,
-							api_consultant_list.email,							
-							api_consultant_list.serial_no,
 							api_consultant_general.lname,
-							api_consultant_general.source,
-							api_consultant_general.sourcetype,
-							api_consultant_list.jobboards_keywords
+							api_consultant_list.email,
+							api_consultant_list.jobboards_keywords,
+							'' AS country,
+							'' AS state,
+							'' AS zip,
+							'' AS miles
 						      FROM api_consultant_list,
 							api_consultant_general
 							WHERE api_consultant_list.username=api_consultant_general.username
 							AND api_consultant_general.sourcetype = 169
 							AND api_consultant_list.email != ''
 							AND api_consultant_list.jobboards_optin_email = 'Y'
-							AND api_consultant_list.jobboards_keywords != ''";
+							AND api_consultant_list.jobboards_keywords != ''
+						    UNION
+						    SELECT
+                                                        talentpool_list.fname,
+							talentpool_list.lname,
+							talentpool_list.email,
+							talentpool_list.job_alerts AS jobboards_keywords,
+							talentpool_list.country AS country,
+							talentpool_list.state AS state,
+                                                        talentpool_list.zip AS zip,
+                                                        talentpool_list.miles AS miles
+						      FROM talentpool_list
+                                                        WHERE talentpool_list.email != ''
+                                                        AND talentpool_list.jobboards_optin_email = 'Y'
+                                                        AND talentpool_list.job_alerts != ''";
 			
 			$applicant_search_rs = mysql_query($applicant_search_query,$db);	
 			
 			while($applicant_search_row = mysql_fetch_assoc($applicant_search_rs))
-			{
-				$applicants_arr['username'][] = $applicant_search_row['username'];
+			{				
 				$applicants_arr['lname'][] = $applicant_search_row['lname'];
 				$applicants_arr['email'][] = $applicant_search_row['email'];
-				$applicants_arr['jobboards_keywords'][] = $applicant_search_row['jobboards_keywords'];				
+				$applicants_arr['jobboards_keywords'][] = $applicant_search_row['jobboards_keywords'];
+				$applicants_arr['country'][] = $applicant_search_row['country'];
+				$applicants_arr['state'][] = $applicant_search_row['state'];
+				$applicants_arr['zip'][] = $applicant_search_row['zip'];
+				$applicants_arr['miles'][] = $applicant_search_row['miles'];
 			}
 			
 			$applicants_arr_length = count($applicants_arr['email']);
 			for($i=0; $i< $applicants_arr_length; $i++)
-			{			
+			{
+				$zipCodesInProximity = "";
+				$zipCodesInProximityStr = "";
+				$whereCond = "";
+				
 				// Finds whether keywords saved by applicant coming from job boards matches the job orders' ids posted to website in the past 5 mins -  searches in posdesc.search_data column in DB				
 				$jobboards_searchstr = searchSubResume($applicants_arr['jobboards_keywords'][$i]);
 				$recent_jo_ids = implode(',',$recent_jo_arr);
 				
+				// Check if preferences for miles set
+				if(!empty($applicants_arr['miles'][$i]) && $applicants_arr['miles'][$i]!=0){
+					$zipCodesInProximity = getZipCodeRadius($applicants_arr['zip'][$i],$applicants_arr['miles'][$i],$applicants_arr['country'][$i]);
+					$zipCodesInProximityStr = implode(',',$zipCodesInProximity);
+					if(!empty($zipCodesInProximityStr)){
+						$whereCond = " AND zipcode in (".$zipCodesInProximityStr.")";
+					}
+				}else{
+					// Check if preferences for state set
+					if(!empty($applicants_arr['state'][$i])){					
+						$whereCond = " AND state LIKE '%".$applicants_arr['state'][$i]."%'";					
+					}
+					// Check if preferences for country set
+					if(!empty($applicants_arr['country'][$i])){					
+						$whereCond .= " AND country = '".$applicants_arr['country'][$i]."'";					
+					}
+				}
 				$jobboards_keywords_match_query = "SELECT posid, postitle, posdesc, requirements, post_job_chk, posted_date, company, location, joblocation FROM posdesc WHERE MATCH (posdesc.search_data) AGAINST ('".$jobboards_searchstr."' IN BOOLEAN MODE) AND posdesc.posid in (".$recent_jo_ids.")";
 				$jobboards_keywords_match_rs = mysql_query($jobboards_keywords_match_query,$db);
 				while($jobboards_keywords_match_row = mysql_fetch_assoc($jobboards_keywords_match_rs))
@@ -106,10 +145,15 @@
 					$jo_companyname = $company_name_row[0];			
 					
 					// Get Company Location
-					$company_location_query ="select CONCAT(title,' - ',address1,' ',address2,' ',city,' ',state,' ',zipcode) company_location from staffoppr_location where sno='".$jobboards_keywords_match_row['location']."' AND ltype in ('com','loc') AND status='A'";
+					$company_location_query ="select CONCAT(title,' - ',address1,' ',address2,' ',city,' ',state,' ',zipcode) company_location from staffoppr_location where sno='".$jobboards_keywords_match_row['location']."' AND ltype in ('com','loc') AND status='A' ".$whereCond;
 					$company_location_rs = mysql_query($company_location_query,$db);
 					$company_location_row = mysql_fetch_row($company_location_rs);
 					$jo_location = $company_location_row[0];
+					
+					// Donot send mail alerts if applicant's preferred job location zipcode not within X radius
+					if(empty($jo_location) && $whereCond != ""){
+						break;
+					}
 					
 					// Sends e-mail alert to applicant
 					$mailtype = "text/html";
@@ -122,7 +166,9 @@
 							"\n Company Name: ".$jo_companyname.
 							"\n Job Location: ".$jo_job_location.$jo_location;
 							
-					$message .= "\n\n\n Disclaimer: You have received this mail because you are a registered member on ".$jo_companyname.". This is a system generated email, please don't reply to this message. If you do not want to receive this mailer, <a href='#'>Unsubscribe</a>";
+					$unsubscibe_link = "https://".$companyuser.".akken.com/unsubscribe/".strtolower($to);
+							
+					$message .= "\n\n\n Disclaimer: You have received this mail because you are a registered member on ".$jo_companyname.". This is a system generated email, please don't reply to this message. If you do not want to receive this mailer, <a target=_blank href='".$unsubscibe_link."'>Unsubscribe</a>";
 					$message = "<div style='font-family: arial; font-size: 10pt;'>".nl2br($message)."</div>";
 					
 					$ato = explode(",",$to);
@@ -142,4 +188,53 @@
 		 * END OF CODE
 		 */	
 	}
+	
+	/*
+	 *  Finds the nearest zipcodes within proximity of user's specified zipcode and radius preferences
+	 */
+	function getZipCodeRadius($zipcode,$zipmiles,$zipCountry)
+	{
+		global $maindb;
+		
+		$questr = "";
+		$zipCodes = array();
+		
+		// Checks whether Country selected is United States (code = 243) or Canada (code = 42)
+		if($zipCountry == "243")
+			$questr	= " AND Country='U'";
+		else if($zipCountry == "42")
+			$questr	= " AND Country='C'";
+		else
+			$questr = "";
+		
+		$que = "SELECT Latitude,Longitude FROM zipcodedb WHERE ZipCode = '$zipcode'".$questr;
+		$res = mysql_query($que, $maindb);
+		$row = mysql_fetch_assoc($res);
+
+		$zipCodeLatitude 	= $row['Latitude'];
+		$zipCodeLongitude 	= $row['Longitude'];		
+		
+		$iDistance = $zipmiles; // user preferred zipcode radius in miles
+		//$iRadius = 6371; // earth radius in km
+		$iRadius = 3958; // earth radius in miles
+		$fLat = $zipCodeLatitude; // user preferred zipcode's latitude
+		$fLon = $zipCodeLongitude; // user preferred zipcode's longitude		
+		
+		$selQuery = "SELECT
+				Latitude, Longitude, ZipCode,
+				$iRadius * 2 * ASIN(SQRT(POWER(SIN(( $fLat - ABS(Latitude)) * PI() / 180 / 2),2) + COS( $fLat * PI()/180) * COS(ABS(Latitude) * PI() / 180) * POWER(SIN(( $fLon - Longitude) * PI() / 180 / 2), 2) )) AS distance
+				FROM zipcodedb
+				HAVING distance < $iDistance 
+				ORDER BY distance";
+				
+		$resGeo = mysql_query($selQuery, $maindb);
+		while($rowGeo = mysql_fetch_assoc($resGeo))
+		{
+			$zipCodes[] = $rowGeo['ZipCode'];			
+		}
+		$zipCodes = array_unique($zipCodes);
+		return $zipCodes;			
+			      
+	}
+	
 ?>
