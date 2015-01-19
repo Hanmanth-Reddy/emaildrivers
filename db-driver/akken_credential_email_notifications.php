408cf9d7164c81669bd1656b800115647fe1baf7
prasada <prasada@755fa31f-b065-4e3e-93bf-f78d3898b98b>
2015-01-19 04:18:12 +0000

    committing AKKENED_7_5_0_51 tag in to trunk

--- db-driver/akken_credential_email_notifications.php
+++ db-driver/akken_credential_email_notifications.php
@@ -3,10 +3,21 @@
 	ini_set("include_path",$include_path);
 
 	require("global.inc");
-
+	
+	$main_query = ""; //variable for main query.
+	
 	$roles_query = ""; //variable for roles query.
+	$roles_cand_query = ""; //variable for roles query.
+	$roles_emp_query = ""; //variable for roles query.
+	
 	$owner_query = ""; //variable for owner query.
+	$owner_cand_query = ""; //variable for owner query.
+	$owner_emp_query = ""; //variable for owner query.
+	
 	$people_query = ""; //variable for people query.
+	$people_cand_query = ""; //variable for people query.
+	$people_emp_query = ""; //variable for people query.
+	
 	$email_flag = 'N'; //default value for email alert in notification table.
 	$sms_flag = 'N'; //default value for sms alert in notification table.
 	$popup_flag = 'N'; // //default value for popup alert in notification table.
@@ -22,9 +33,31 @@
 			// Fetch the Companies one by one
 			$companyuser=strtolower($drow[0]);
 			require("database.inc");
+			
+			$notify_to_list = "";
+			$people_list = "";
+			$notify_time = array();
+			$notify_to_val = array();
+			$credentials_assoc_with = array();
+			
+			$main_query = ""; //variable for main query.
+			
+			$roles_query = ""; //variable for roles query.
+			$roles_cand_query = ""; //variable for roles query.
+			$roles_emp_query = ""; //variable for roles query.
+			
+			$owner_query = ""; //variable for owner query.
+			$owner_cand_query = ""; //variable for owner query.
+			$owner_emp_query = ""; //variable for owner query.
+			
+			$people_query = ""; //variable for people query.
+			$people_cand_query = ""; //variable for people query.
+			$people_emp_query = ""; //variable for people query.
+			
+			$date_expr = ""; // Date Expression for notification frequency.
 
 			//QUERY TO FETCH NOTIFICATION SETTINGS DATA	
-			$notification_settings="select notify_time,status,notify_mode,notify_to,notify_people from notifications_settings  where status='1' and mod_id='credential' and notify_status = 'ACTIVE'";
+			$notification_settings="select notify_time,status,notify_mode,notify_to,notify_people,credentials_assoc_with from notifications_settings  where status='1' and mod_id='credential' and notify_status = 'ACTIVE'";
 			$notification_result=mysql_query($notification_settings,$db);
 			if(mysql_num_rows($notification_result)==1)
 			{
@@ -33,6 +66,7 @@
 				$notify_to_list  = $rrow['notify_to'];
 				$notify_to_val = explode(",",$notify_to_list);
 				$people_list = $rrow['notify_people'];
+				$credentials_assoc_with = explode(",",$rrow['credentials_assoc_with']);
 
 				/* Conditions to check selected time frequency. According to that will create a phrase for query. if multiple then will create a phrase with OR condition.*/
 
@@ -89,27 +123,152 @@
 
 					if(in_array('1',$notify_to_val))
 					{
-						$roles_query = "SELECT  emp.username,date_format(cc.valid_to,'%m/%d/%Y'),mct.credential_type,mcn.credential_name,cl.fname,cl.lname,emp.name,cc.cre_number,date_format(cc.valid_from,'%m/%d/%Y') as valid_from,cc.id,IF(hg.mobile IS NULL,'',hg.mobile) mobile,IF(hg.email='',IF(hg.alternate_email='',IF(hg.other_email='',' ',hg.other_email),hg.alternate_email),hg.email) email,IF(hg.esms='N','',hg.mser_domain) smsdomain
-						FROM hrcon_general hg,entity_roles el, emp_list emp, candidate_list cl, candidate_credentials cc
-						LEFT JOIN manage_credentials_name mcn ON cc.cre_name_id = mcn.id
-						LEFT JOIN manage_credentials_type mct ON cc.cre_type_id = mct.id
-						WHERE el.empId = emp.username AND cc.cand_sno = cl.sno AND cc.cand_sno = el.entityId AND cl.status = 'ACTIVE' AND hg.username = emp.username AND (".$date_expr.")";
+						if(in_array("1",$credentials_assoc_with))
+						{
+							$roles_cand_query = "SELECT  emp.username,date_format(cc.valid_to,'%m/%d/%Y'),mct.credential_type,mcn.credential_name,cl.fname,cl.lname,emp.name,cc.cre_number,date_format(cc.valid_from,'%m/%d/%Y') as valid_from,cc.id,IF(hg.mobile IS NULL,'',hg.mobile) mobile,IF(hg.email='',IF(hg.alternate_email='',IF(hg.other_email='',' ',hg.other_email),hg.alternate_email),hg.email) email,IF(hg.esms='N','',hg.mser_domain) smsdomain, 'Candidate' AS credential_candemp
+							FROM hrcon_general hg,entity_roles el, emp_list emp, candidate_list cl, candidate_credentials cc
+							LEFT JOIN manage_credentials_name mcn ON cc.cre_name_id = mcn.id
+							LEFT JOIN manage_credentials_type mct ON cc.cre_type_id = mct.id
+							WHERE el.empId = emp.username AND cc.cand_sno = cl.sno AND cc.cand_sno = el.entityId AND cl.status = 'ACTIVE' AND cc.status = 'ACTIVE' AND hg.username = emp.username AND (".$date_expr.")";
+						}
+						
+						if(in_array("2",$credentials_assoc_with))
+						{
+							$roles_emp_query = "SELECT hg.username,DATE_FORMAT(emp2.valid_to,'%m/%d/%Y'),emp2.credential_type,emp2.credential_name,emp2.fname,emp2.lname,CONCAT(hg.fname,' ',hg.lname),emp2.cre_number,DATE_FORMAT(emp2.valid_from,'%m/%d/%Y') AS valid_from,emp2.id,IF(hg.mobile IS NULL,'',hg.mobile)    mobile,IF(hg.email='',IF(hg.alternate_email='',IF(hg.other_email='',' ',hg.other_email),hg.alternate_email),hg.email)    email,IF(hg.esms='N','',hg.mser_domain)    smsdomain,'Employee' AS credential_candemp
+							FROM hrcon_general hg,
+								(SELECT hc.valid_to, mct.credential_type, mcn.credential_name, hg_temp.fname, hg_temp.lname, el.name, hc.cre_number, hc.valid_from, hc.id, ac.person roleid FROM hrcon_credentials hc
+								LEFT JOIN manage_credentials_name mcn
+								  ON hc.cre_name_id = mcn.id
+								LEFT JOIN manage_credentials_type mct
+								  ON hc.cre_type_id = mct.id, emp_list el 
+								LEFT JOIN empcon_jobs ej
+								  ON (el.username = ej.username AND ej.jtype != '' AND ej.jotype != '0' AND ej.assg_status IN('approved')) 
+								LEFT JOIN assign_commission ac 
+								  ON (ac.assignid = ej.sno AND ac.assigntype='E') 
+								LEFT JOIN hrcon_compen hcomp
+								  ON (el.username = hcomp.username AND hcomp.ustatus = 'active'), hrcon_general hg_temp
+								WHERE el.sno = hc.app_sno 								 
+								  AND el.lstatus NOT IN ('DA','INACTIVE')
+								  AND el.empterminated != 'Y'
+								  AND hc.status = 'ACTIVE'
+								  AND hc.ustatus = 'active' 
+								  AND ac.person IS NOT NULL 
+								  AND el.username = hg_temp.username
+								  AND (".$date_expr.")
+								GROUP BY ac.person, hc.id 
+								ORDER BY ac.person) emp2
+							WHERE emp2.roleid = hg.username ";
+						}
+										
+						if($roles_cand_query != ""){
+							$roles_query = $roles_cand_query;
+						}
+	  
+						if($roles_emp_query != ""){
+							$roles_query = $roles_emp_query;
+						}
+						
+						if($roles_cand_query != "" && $roles_emp_query != ""){
+							$roles_query = $roles_cand_query." UNION ".$roles_emp_query;
+						}
 					}
 					if(in_array('2',$notify_to_val)) 
 					{
-						$owner_query .= "SELECT  emp.username,date_format(cc.valid_to,'%m/%d/%Y'),mct.credential_type,mcn.credential_name,cl.fname,cl.lname,emp.name,cc.cre_number,date_format(cc.valid_from,'%m/%d/%Y') as valid_from,cc.id,IF(hg.mobile IS NULL,'',hg.mobile) mobile,IF(hg.email='',IF(hg.alternate_email='',IF(hg.other_email='',' ',hg.other_email),hg.alternate_email),hg.email) email,IF(hg.esms='N','',hg.mser_domain) smsdomain
-						FROM hrcon_general hg, candidate_list cl, emp_list emp, candidate_credentials cc
-						LEFT JOIN manage_credentials_name mcn ON cc.cre_name_id = mcn.id
-						LEFT JOIN manage_credentials_type mct ON cc.cre_type_id = mct.id
-						WHERE  cl.sno = cc.cand_sno AND cl.owner = emp.username AND cl.status = 'ACTIVE' AND hg.username = emp.username AND (".$date_expr.")"; 
+						if(in_array("1",$credentials_assoc_with))
+						{
+							$owner_cand_query .= "SELECT  emp.username,date_format(cc.valid_to,'%m/%d/%Y'),mct.credential_type,mcn.credential_name,cl.fname,cl.lname,emp.name,cc.cre_number,date_format(cc.valid_from,'%m/%d/%Y') as valid_from,cc.id,IF(hg.mobile IS NULL,'',hg.mobile) mobile,IF(hg.email='',IF(hg.alternate_email='',IF(hg.other_email='',' ',hg.other_email),hg.alternate_email),hg.email) email,IF(hg.esms='N','',hg.mser_domain) smsdomain, 'Candidate' AS credential_candemp
+							FROM hrcon_general hg, candidate_list cl, emp_list emp, candidate_credentials cc
+							LEFT JOIN manage_credentials_name mcn ON cc.cre_name_id = mcn.id
+							LEFT JOIN manage_credentials_type mct ON cc.cre_type_id = mct.id
+							WHERE  cl.sno = cc.cand_sno AND cl.owner = emp.username AND cl.status = 'ACTIVE' AND cc.status = 'ACTIVE' AND hg.username = emp.username AND (".$date_expr.")";
+						}
+						
+						if(in_array("2",$credentials_assoc_with))
+						{
+							$owner_emp_query = "SELECT hg.username,DATE_FORMAT(emp2.valid_to,'%m/%d/%Y'),emp2.credential_type,emp2.credential_name,emp2.fname,emp2.lname,CONCAT(hg.fname,' ',hg.lname),emp2.cre_number,DATE_FORMAT(emp2.valid_from,'%m/%d/%Y') AS valid_from,emp2.id,IF(hg.mobile IS NULL,'',hg.mobile)    mobile,IF(hg.email='',IF(hg.alternate_email='',IF(hg.other_email='',' ',hg.other_email),hg.alternate_email),hg.email)    email,IF(hg.esms='N','',hg.mser_domain)    smsdomain,'Employee' AS credential_candemp
+							FROM hrcon_general hg,
+								(SELECT hc.valid_to,mct.credential_type,mcn.credential_name,hg_temp.fname,hg_temp.lname,el.name,hc.cre_number,hc.valid_from,hc.id,el.approveuser OWNER
+								FROM hrcon_credentials hc
+								   LEFT JOIN manage_credentials_name mcn
+								     ON hc.cre_name_id = mcn.id
+								   LEFT JOIN manage_credentials_type mct
+								     ON hc.cre_type_id = mct.id,
+								   emp_list el
+								   LEFT JOIN hrcon_compen hcomp
+								     ON (el.username = hcomp.username
+									 AND hcomp.ustatus = 'active'),
+								   hrcon_general hg_temp
+								WHERE el.sno = hc.app_sno								 
+								     AND el.lstatus NOT IN ('DA','INACTIVE')
+								     AND el.empterminated != 'Y'
+								     AND hc.status = 'ACTIVE'
+								     AND hc.ustatus = 'active'     
+								     AND el.username = hg_temp.username
+								     AND (".$date_expr.")
+								GROUP BY hc.id) emp2
+							WHERE emp2.owner = hg.username";
+						}
+								      
+						if($owner_cand_query != ""){
+							$owner_query = $owner_cand_query;
+						}
+	  
+						if($owner_emp_query != ""){
+							$owner_query = $owner_emp_query;
+						}
+						
+						if($owner_cand_query != "" && $owner_emp_query != ""){
+							$owner_query = $owner_cand_query." UNION ".$owner_emp_query;
+						}
 					}
 					if(in_array('3',$notify_to_val))
 					{
-						$people_query = "SELECT  emp.username,date_format(cc.valid_to,'%m/%d/%Y'),mct.credential_type,mcn.credential_name,cl.fname,cl.lname,emp.name,cc.cre_number,date_format(cc.valid_from,'%m/%d/%Y') as valid_from,cc.id,IF(hg.mobile IS NULL,'',hg.mobile) mobile,IF(hg.email='',IF(hg.alternate_email='',IF(hg.other_email='',' ',hg.other_email),hg.alternate_email),hg.email) email,IF(hg.esms='N','',hg.mser_domain) smsdomain
-						FROM hrcon_general hg, emp_list emp,candidate_list cl,candidate_credentials cc 
-						LEFT JOIN manage_credentials_name mcn ON cc.cre_name_id = mcn.id
-						LEFT JOIN manage_credentials_type mct ON cc.cre_type_id = mct.id
-						where  emp.username IN (".$people_list.") AND cc.cand_sno = cl.sno AND cl.status = 'ACTIVE' AND hg.username = emp.username AND (".$date_expr.")";
+						if(in_array("1",$credentials_assoc_with))
+						{
+							$people_cand_query = "SELECT  emp.username,date_format(cc.valid_to,'%m/%d/%Y'),mct.credential_type,mcn.credential_name,cl.fname,cl.lname,emp.name,cc.cre_number,date_format(cc.valid_from,'%m/%d/%Y') as valid_from,cc.id,IF(hg.mobile IS NULL,'',hg.mobile) mobile,IF(hg.email='',IF(hg.alternate_email='',IF(hg.other_email='',' ',hg.other_email),hg.alternate_email),hg.email) email,IF(hg.esms='N','',hg.mser_domain) smsdomain, 'Candidate' AS credential_candemp
+							FROM hrcon_general hg, emp_list emp,candidate_list cl,candidate_credentials cc 
+							LEFT JOIN manage_credentials_name mcn ON cc.cre_name_id = mcn.id
+							LEFT JOIN manage_credentials_type mct ON cc.cre_type_id = mct.id
+							where  emp.username IN (".$people_list.") AND cc.cand_sno = cl.sno AND cl.status = 'ACTIVE' AND cc.status = 'ACTIVE' AND hg.username = emp.username AND (".$date_expr.")";
+						}
+						
+						if(in_array("2",$credentials_assoc_with))
+						{
+							$people_emp_query = "SELECT hg.username,DATE_FORMAT(emp2.valid_to,'%m/%d/%Y'),emp2.credential_type,emp2.credential_name,emp2.fname,emp2.lname,CONCAT(hg.fname,' ',hg.lname),emp2.cre_number,DATE_FORMAT(emp2.valid_from,'%m/%d/%Y') AS valid_from,emp2.id,IF(hg.mobile IS NULL,'',hg.mobile)    mobile,IF(hg.email='',IF(hg.alternate_email='',IF(hg.other_email='',' ',hg.other_email),hg.alternate_email),hg.email)    email,IF(hg.esms='N','',hg.mser_domain)    smsdomain,'Employee' AS credential_candemp
+							FROM hrcon_general hg,
+								(SELECT hc.valid_to,mct.credential_type,mcn.credential_name,hg_temp.fname,hg_temp.lname,el.name,hc.cre_number,hc.valid_from,hc.id     
+								 FROM hrcon_credentials hc
+								   LEFT JOIN manage_credentials_name mcn
+								     ON hc.cre_name_id = mcn.id
+								   LEFT JOIN manage_credentials_type mct
+								     ON hc.cre_type_id = mct.id,
+								   emp_list el
+								   LEFT JOIN hrcon_compen hcomp
+								     ON (el.username = hcomp.username
+									 AND hcomp.ustatus = 'active'),
+								   hrcon_general hg_temp
+								WHERE el.sno = hc.app_sno								     
+								     AND el.lstatus NOT IN ('DA','INACTIVE')
+								     AND el.empterminated != 'Y'
+								     AND hc.status = 'ACTIVE'
+								     AND hc.ustatus = 'active'     
+								     AND el.username = hg_temp.username
+								     AND (".$date_expr.")
+								GROUP BY hc.id) emp2
+							WHERE hg.username IN (".$people_list.")";
+						}
+								      
+						if($people_cand_query != ""){
+							$people_query = $people_cand_query;
+						}
+	  
+						if($people_emp_query != ""){
+							$people_query = $people_emp_query;
+						}
+						
+						if($people_cand_query != "" && $people_emp_query != ""){
+							$people_query = $people_cand_query." UNION ".$people_emp_query;
+						}
 					}
 
 					if($people_query !="")
@@ -133,8 +292,8 @@
 					if($people_query!="" and $roles_query!="" and $owner_query!="")
 						$main_query = $people_query." UNION ".$roles_query ." UNION ".$owner_query;
 
-					// VARIABLES USED TO AVOID MULTIPLE ENTRIES IN NOTIFICATIONS TABLE -- Credential ID stored temporarily.
-					$temp_notification_cred_id = "";
+					// VARIABLES USED TO AVOID MULTIPLE ENTRIES IN NOTIFICATIONS TABLE					
+					$temp_notification_cred_ids = array(); //Credential IDs with notification id stored temporarily.
 
 					//Last inserted id generated from notifications table stored temporarily.
 					$temp_notification_psno = "";
@@ -146,19 +305,18 @@
 						while($email_array = mysql_fetch_array($result_email))
 						{									
 							// AVOID MULTIPLE ENTRIES IN NOTIFICATIONS TABLE
-							if($temp_notification_cred_id !=  $email_array[9])
-							{
-								$temp_notification_psno = "";
-								$subject = "Credential Expiry Alert! - ".$email_array['fname']." ".$email_array['lname']." - ".$email_array['credential_type'];
+							if(!isset($temp_notification_cred_ids[$email_array[9]."_".$email_array['credential_candemp']]))
+							{							
+								$subject = $email_array['credential_candemp']." Credential Expiry Alert! - ".$email_array['fname']." ".$email_array['lname']." - ".$email_array['credential_type'];
 
 								// Message content for Email
-								$matter = "Hello,<br><br>Below Candidate(s) Credentials are nearing expiry.<br><br>Candidate Name : ".$email_array[4]." ".$email_array[5]."<br>Credential Type : ".$email_array[2]."<br>Credential Name : ".$email_array[3]."<br>Credential Number : ".$email_array[7]."<br>Valid From : ".$email_array[8]."<br>Valid To : ".$email_array[1]."<br>";
+								$matter = "Hello,<br><br>Below ".$email_array['credential_candemp']."(s) Credentials are nearing expiry.<br><br>".$email_array['credential_candemp']." Name : ".$email_array[4]." ".$email_array[5]."<br>Credential Type : ".$email_array[2]."<br>Credential Name : ".$email_array[3]."<br>Credential Number : ".$email_array[7]."<br>Valid From : ".$email_array[8]."<br>Valid To : ".$email_array[1]."<br>";
 
 								// replaces <br> tags with newline character as same data used for mail and popup.
 								$matter  = str_replace('<br>','\n',$matter);
 
 								// Message content for SMS - Max 160 chars in SMS
-								$sms_matter = "Credential of Candidate: ".$email_array[4]." having Cred No.: ".$email_array[7]." is going to expire soon. ";
+								$sms_matter = "Credential of ".$email_array['credential_candemp'].": ".$email_array[4]." having Cred No.: ".$email_array[7]." is going to expire on ".$email_array[1];								
 
 								//Call to procedure for inserting to notifications table of akken_notifications.
 								$akken_insert_notification = "call akken_notifications.insertNotification ('".$companyuser."','credential','".$email_array[9]."','".$subject."','".mysql_real_escape_string($matter)."','Active','".mysql_real_escape_string($sms_matter)."',@psno)";
@@ -168,18 +326,22 @@
 								$fetch_psno_result = mysql_fetch_array($res);
 								$psno = $fetch_psno_result[0];
 
-								//Update the flags to the table 
+								//Update the flags in the table 
 								$alert_type_set_query = "UPDATE akken_notifications.notifications SET alert = '".$popup_flag."', email = '".$email_flag."', sms = '".$sms_flag."' WHERE sno = ".$psno."";
 								mysql_query($alert_type_set_query,$db);
 
-								$temp_notification_cred_id =  $email_array[9]; // set this to the current credential id
-								$temp_notification_psno = $psno;
+								$temp_notification_cred_ids[$email_array[9]."_".$email_array['credential_candemp']] = $psno; // set this to the current credential id
 							}
 							// END OF BLOCK TO AVOID MULTIPLE ENTRIES IN NOTIFICATIONS TABLE
 
-							//Call to procedure for inserting to notifications_list table of akken_notifications.
-							$akken_insert_notificationList = "call akken_notifications.insertNotificationList(".$temp_notification_psno.",'".$email_array[0]."','".$email_array['email']."','".$email_array['mobile']."','".$email_array['smsdomain']."');";
-							mysql_query($akken_insert_notificationList,$db);
+							// Get Notification ID based on Credemtial ID set earlier
+							$temp_notification_psno = $temp_notification_cred_ids[$email_array[9]."_".$email_array['credential_candemp']];
+														
+							if(!empty($temp_notification_psno)){
+								//Call to procedure for inserting to notifications_list table of akken_notifications.
+								$akken_insert_notificationList = "call akken_notifications.insertNotificationList(".$temp_notification_psno.",'".$email_array[0]."','".$email_array['email']."','".$email_array['mobile']."','".$email_array['smsdomain']."');";
+								mysql_query($akken_insert_notificationList,$db);
+							}
 
 							$email_array = "";
 						}
