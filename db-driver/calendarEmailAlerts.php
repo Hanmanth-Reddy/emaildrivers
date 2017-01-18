30f4b338779e525703945699c6862195f605ed51
hanmanth <hanmanth@755fa31f-b065-4e3e-93bf-f78d3898b98b>
2017-01-18 08:37:59 +0000

    Committing AKKENED_7_5_0_103 tag in to trunk.

--- db-driver/calendarEmailAlerts.php
+++ db-driver/calendarEmailAlerts.php
@@ -44,47 +44,54 @@
 	{
 		$companyuser=strtolower($drow[0]);
 		require("database.inc");
-
-		$uque="SELECT users.username, timezone.phpvar, orgsetup.notify_email, orgsetup.notify_val FROM users LEFT JOIN orgsetup ON users.username=orgsetup.userid LEFT JOIN timezone ON orgsetup.timezone=timezone.sno WHERE users.usertype!='' AND users.status!='DA' AND orgsetup.notify_email!=''";
+		$uque="SELECT users.username, timezone.phpvar, orgsetup.notify_email, orgsetup.notify_val,su.collaboration 
+				FROM users 
+				LEFT JOIN orgsetup ON users.username=orgsetup.userid 
+				LEFT JOIN timezone ON orgsetup.timezone=timezone.sno
+		        LEFT JOIN sysuser su ON users.username=su.username 
+		        WHERE users.usertype!='' AND users.status!='DA' AND orgsetup.notify_email!=''";
 		$ures=mysql_query($uque,$db);
 		while($urow=mysql_fetch_row($ures))
 		{
-			$username = $urow[0];
-			$notify_to = $urow[2];
-			$notify_val = $urow[3];
+			$collaborationVal = $urow[4];
+			if($collaborationVal!='NO' && strpos($collaborationVal,"+10+")!==false){
+				$username = $urow[0];
+				$notify_to = $urow[2];
+				$notify_val = $urow[3];
 
-			if(in_array($urow[1],$us_tz))
-				$usertime = $starttime + $us_tz_value;
-			else
-				$usertime = $starttime + $non_us_tz_value;
+				if(in_array($urow[1],$us_tz))
+					$usertime = $starttime + $us_tz_value;
+				else
+					$usertime = $starttime + $non_us_tz_value;
 
-			$utzos = getUserSTZOffset();
-			$qsdatetime = $usertime - $slot_time;
-			$qedatetime = $usertime;
+				$utzos = getUserSTZOffset();
+				$qsdatetime = $usertime - $slot_time;
+				$qedatetime = $usertime;
 
-			$nque="SELECT appointments.title, DATE_FORMAT(FROM_UNIXTIME(IF(appointments.recurrence='none',(appointments.sdatetime + '$utzos'),(recurrences.otime + '$utzos'))),'%W, %m/%d/%Y %h:%i%p') stime, DATE_FORMAT(FROM_UNIXTIME(IF(appointments.recurrence='none',(appointments.edatetime + '$utzos'),(recurrences.etime + '$utzos'))),'%W, %m/%d/%Y %h:%i%p') etime, appointments.descri FROM appointments LEFT JOIN recurrences ON recurrences.ano = appointments.sno WHERE appointments.status='active' AND (appointments.username='".$username."' OR FIND_IN_SET('".$username."',appointments.approved) > 0 OR FIND_IN_SET('".$username."',appointments.tentative) > 0 OR FIND_IN_SET('".$username."',appointments.pending) > 0) AND ((appointments.recurrence='none' AND ((appointments.sdatetime - $notify_val)>=$qsdatetime AND (appointments.sdatetime - $notify_val)<=$qedatetime)) OR (appointments.recurrence='recurrence' AND ((recurrences.otime - $notify_val)>=$qsdatetime AND (recurrences.otime - $notify_val)<=$qedatetime))) ORDER BY stime,appointments.title ASC";
-			$nres=mysql_query($nque,$db);
-			while($nrow=mysql_fetch_row($nres))
-			{
-				$to = $notify_to;
-				$nval = $notify_val/60;
-				$ato = explode(",",$to);
+				$nque="SELECT appointments.title, DATE_FORMAT(FROM_UNIXTIME(IF(appointments.recurrence='none',(appointments.sdatetime + '$utzos'),(recurrences.otime + '$utzos'))),'%W, %m/%d/%Y %h:%i%p') stime, DATE_FORMAT(FROM_UNIXTIME(IF(appointments.recurrence='none',(appointments.edatetime + '$utzos'),(recurrences.etime + '$utzos'))),'%W, %m/%d/%Y %h:%i%p') etime, appointments.descri FROM appointments LEFT JOIN recurrences ON recurrences.ano = appointments.sno WHERE appointments.status='active' AND (appointments.username='".$username."' OR FIND_IN_SET('".$username."',appointments.approved) > 0 OR FIND_IN_SET('".$username."',appointments.tentative) > 0 OR FIND_IN_SET('".$username."',appointments.pending) > 0) AND ((appointments.recurrence='none' AND ((appointments.sdatetime - $notify_val)>=$qsdatetime AND (appointments.sdatetime - $notify_val)<=$qedatetime)) OR (appointments.recurrence='recurrence' AND ((recurrences.otime - $notify_val)>=$qsdatetime AND (recurrences.otime - $notify_val)<=$qedatetime))) ORDER BY stime,appointments.title ASC";
+				$nres=mysql_query($nque,$db);
+				while($nrow=mysql_fetch_row($nres))
+				{
+					$to = $notify_to;
+					$nval = $notify_val/60;
+					$ato = explode(",",$to);
 
-				$subject = "Event Reminder: ".$nrow[0];
-				$notify_body="Your event <b>".$nrow[0]."</b>, will start in <b>".$nval."</b> mins"."<BR><BR><b>Start Time: </b>".$nrow[1]."<br>"."<b>End Time: </b>".$nrow[2];
+					$subject = "Event Reminder: ".$nrow[0];
+					$notify_body="Your event <b>".$nrow[0]."</b>, will start in <b>".$nval."</b> mins"."<BR><BR><b>Start Time: </b>".$nrow[1]."<br>"."<b>End Time: </b>".$nrow[2];
 
-				if(trim($nrow[3])!="")
+					if(trim($nrow[3])!="")
 					$notify_body.="<BR><b>Description:</b><BR><BR>".$nrow[3];
 
-				$matter = "<div style='font-family: arial; font-size: 10pt;'>".str_replace("\n","<br>",$notify_body)."<br><br></div>";
+					$matter = "<div style='font-family: arial; font-size: 10pt;'>".str_replace("\n","<br>",$notify_body)."<br><br></div>";
 
-				if($to!="")
-				{
-					$mailheaders=array("Date: $curtime_header","From: $from","To: $to","Subject: $subject","MIME-Version: 1.0");
-					$msg_body=prepareBodyA($matter,$mailheaders,$mailtype);
-					$smtp->SendMessage($from,$ato,$mailheaders,$msg_body);
+					if($to!="")
+					{
+						$mailheaders=array("Date: $curtime_header","From: $from","To: $to","Subject: $subject","MIME-Version: 1.0");
+						$msg_body=prepareBodyA($matter,$mailheaders,$mailtype);
+						$smtp->SendMessage($from,$ato,$mailheaders,$msg_body);
+					}
 				}
-			}
+			}	
 		}
 	}
 ?>
