ef4fd44e0513a00b1d775f8bbb1d9e7a43300d64
hanmanth <hanmanth@755fa31f-b065-4e3e-93bf-f78d3898b98b>
2017-01-18 08:37:59 +0000

    Committing AKKENED_7_5_0_103 tag in to trunk.

--- trunk/db-driver/calendarAlerts.php
+++ trunk/db-driver/calendarAlerts.php
@@ -36,39 +36,47 @@
 			$companyuser=strtolower($drow[0]);
 			require("database.inc");
 
-			$uque="select users.username, timezone.phpvar from users LEFT JOIN orgsetup ON users.username=orgsetup.userid LEFT JOIN timezone ON orgsetup.timezone=timezone.sno where users.usertype!='' AND users.status!='DA'";
+			$uque="select users.username, timezone.phpvar,su.collaboration
+					from users 
+					LEFT JOIN orgsetup ON users.username=orgsetup.userid 
+					LEFT JOIN timezone ON orgsetup.timezone=timezone.sno
+					LEFT JOIN sysuser su ON users.username=su.username 
+					where users.usertype!='' AND users.status!='DA'";
 			$ures=mysql_query($uque,$db);
 			while($urow=mysql_fetch_row($ures))
 			{
-				if($urow[1]!="")
-				{
-					$event = "";
-					$username = $urow[0];
-					$channel_name = "chan-".$companyuser."-".$username;
-
-					if(in_array($urow[1],$us_tz))
-						$usertime = $starttime + $us_tz_value;
-					else
-						$usertime = $starttime + $non_us_tz_value;
-
-					$utzos = getUserSTZOffset();
-					$qsdatetime = $usertime - $slot_time;
-					$qedatetime = $usertime;
-
-					$nque="SELECT appointments.title, DATE_FORMAT(FROM_UNIXTIME(IF(appointments.recurrence='none',(appointments.sdatetime + '$utzos'),(recurrences.otime + '$utzos'))),'%W, %m/%d/%Y %h:%i%p') stime, DATE_FORMAT(FROM_UNIXTIME(IF(appointments.recurrence='none',(appointments.edatetime + '$utzos'),(recurrences.etime + '$utzos'))),'%W, %m/%d/%Y %h:%i%p') etime FROM appointments LEFT JOIN recurrences ON recurrences.ano = appointments.sno WHERE appointments.dis='Yes' AND appointments.status='active' AND (appointments.username='".$username."' OR FIND_IN_SET('".$username."',appointments.approved) > 0 OR FIND_IN_SET('".$username."',appointments.tentative) > 0 OR FIND_IN_SET('".$username."',appointments.pending) > 0) AND ((appointments.recurrence='none' AND ((appointments.sdatetime - appointments.rtime)>=$qsdatetime AND (appointments.sdatetime - appointments.rtime)<=$qedatetime)) OR (appointments.recurrence='recurrence' AND ((recurrences.otime - appointments.rtime)>=$qsdatetime AND (recurrences.otime - appointments.rtime)<=$qedatetime))) ORDER BY stime,appointments.title ASC";
-					$nres=mysql_query($nque,$db);
-					while($nrow=mysql_fetch_row($nres))
+				$collaborationVal = $urow[2];
+				if($collaborationVal!='NO' && strpos($collaborationVal,"+10+")!==false){
+					if($urow[1]!="")
 					{
-						if($event=="")
-							$event="Subject : ".addslashes($nrow[0])."|akkenPSplit|"."Start Time : ".$nrow[1]."|akkenPSplit|"."End Time : ".$nrow[2];
+						$event = "";
+						$username = $urow[0];
+						$channel_name = "chan-".$companyuser."-".$username;
+
+						if(in_array($urow[1],$us_tz))
+							$usertime = $starttime + $us_tz_value;
 						else
-							$event.="|akkenESplit|"."Subject : ".addslashes($nrow[0])."|akkenPSplit|"."Start Time : ".$nrow[1]."|akkenPSplit|"."End Time : ".$nrow[2];
-					}
+							$usertime = $starttime + $non_us_tz_value;
 
-					if($event!="")
-					{
-						$cmdop = "ADDMESSAGE ".$channel_name." ".$event."\n";
-						fwrite($meteor, $cmdop);
+						$utzos = getUserSTZOffset();
+						$qsdatetime = $usertime - $slot_time;
+						$qedatetime = $usertime;
+
+						$nque="SELECT appointments.title, DATE_FORMAT(FROM_UNIXTIME(IF(appointments.recurrence='none',(appointments.sdatetime + '$utzos'),(recurrences.otime + '$utzos'))),'%W, %m/%d/%Y %h:%i%p') stime, DATE_FORMAT(FROM_UNIXTIME(IF(appointments.recurrence='none',(appointments.edatetime + '$utzos'),(recurrences.etime + '$utzos'))),'%W, %m/%d/%Y %h:%i%p') etime FROM appointments LEFT JOIN recurrences ON recurrences.ano = appointments.sno WHERE appointments.dis='Yes' AND appointments.status='active' AND (appointments.username='".$username."' OR FIND_IN_SET('".$username."',appointments.approved) > 0 OR FIND_IN_SET('".$username."',appointments.tentative) > 0 OR FIND_IN_SET('".$username."',appointments.pending) > 0) AND ((appointments.recurrence='none' AND ((appointments.sdatetime - appointments.rtime)>=$qsdatetime AND (appointments.sdatetime - appointments.rtime)<=$qedatetime)) OR (appointments.recurrence='recurrence' AND ((recurrences.otime - appointments.rtime)>=$qsdatetime AND (recurrences.otime - appointments.rtime)<=$qedatetime))) ORDER BY stime,appointments.title ASC";
+						$nres=mysql_query($nque,$db);
+						while($nrow=mysql_fetch_row($nres))
+						{
+							if($event=="")
+								$event="Subject : ".addslashes($nrow[0])."|akkenPSplit|"."Start Time : ".$nrow[1]."|akkenPSplit|"."End Time : ".$nrow[2];
+							else
+								$event.="|akkenESplit|"."Subject : ".addslashes($nrow[0])."|akkenPSplit|"."Start Time : ".$nrow[1]."|akkenPSplit|"."End Time : ".$nrow[2];
+						}
+
+						if($event!="")
+						{
+							$cmdop = "ADDMESSAGE ".$channel_name." ".$event."\n";
+							fwrite($meteor, $cmdop);
+						}
 					}
 				}
 			}
